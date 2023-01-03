71b7d79dfdd05d9156783807c8916481352e2492
Christoph <christoph@debian>
2023-01-03 10:00:53 +0100

    create extract routine

--- src/Interpreter/functions.ts
+++ src/Interpreter/functions.ts
@@ -1,5 +1,20 @@
+import { RunTimeError } from "./Error";
+import { VariableStorrage } from "./Interpreter";
 import { expression } from "./types";
 
-export function callFunction(name:string,args:expression){
-    return 0
+//All these function HAVE to return an expression
+export function callFunction(storrage:VariableStorrage,name:string,args:expression[]):expression{
+    return ""
+}
+
+//All these routines don't return anything
+export function callRoutine(storrage:VariableStorrage,name:string,args:expression[]):void{
+    if(name === "extract"){
+        if(args.length != 1) throw new RunTimeError("extracts accepts exactly one argumen",0,0)
+        let arg = args[0]
+        if(typeof arg != "object") throw new RunTimeError("extracts accepts only an object as argument",0,0)
+        Object.entries(arg).forEach(e => {
+            storrage.store(e[0],e[1])
+        })
+    }
 }
